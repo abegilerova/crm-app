c52d4b0ce94e3f56a876a73919b2fc4bc73dd426
abegilerova <abegilerova@gmail.com>
2020-10-23 19:48:04 -0500

    edit, delete to single page

--- frontend/crm-app-frontend/src/pages/CustomerPage.js
+++ frontend/crm-app-frontend/src/pages/CustomerPage.js
@@ -1,12 +1,16 @@
 import React, { useState, useEffect } from 'react'
-import { customerGet } from '../api/CrmAppApi'
-import { Link } from 'react-router-dom'
+import { customerGet, customerDelete } from '../api/CrmAppApi'
+import { useHistory, Link } from 'react-router-dom'
 import { useParams } from 'react-router-dom'
+import Button from 'react-bootstrap/Button'
+import { confirmAlert } from 'react-confirm-alert'
+import 'react-confirm-alert/src/react-confirm-alert.css'
 
 const CustomerPage = () => {
   // const [djangoObject, setDjangoObject] = useState(null)
   const [customer, setCustomer] = useState(null)
   const { pk } = useParams()
+  const history = useHistory()
 
   useEffect(() => {
     customerGet(pk).then(d => setCustomer(d))
@@ -14,9 +18,38 @@ const CustomerPage = () => {
     console.log("customer ", customer)
   }, [pk])
 
+
+  function handleClick(pk, customerName) {
+    history.push(`/customers/${pk}/edit/`, { name: customerName });
+  }
+
+  function handleDelete(pk, customerName) {
+    confirmAlert({
+      title: 'Confirm to delete customer',
+      message: 'Are you sure to do this?',
+      buttons: [
+        {
+          label: 'Yes',
+          onClick: () => {
+            customerDelete(pk)
+            history.push(`/customers/`)
+          }
+
+        },
+        {
+          label: 'No',
+          onClick: () => alert('Click No')
+        }
+      ]
+    })
+  }
+
   return (
     <div>
       <h1> {customer && customer.first_name}</h1>
+      <Button onClick={() => handleClick(customer.pk, customer.first_name)}>Edit</Button>
+
+      <Button onClick={() => handleDelete(customer.pk)}>Delete</Button>
       <ul>
 
       </ul>
